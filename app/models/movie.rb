39e19664792de0e563d1404959340c304f84135d
bmcilhenny <bmcilhen@gmail.com>
2019-06-17 15:00:01 -0400

    use SQL to grab a movie's directors, get_crew method grabs all persons with particular job and department associated with that movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
     has_one :crew
+
+    def directors
+        Person.joins("JOIN person_crews ON person_crews.person_id = people.id AND person_crews.job = 'Director' AND person_crews.department = 'Directing'").joins("INNER JOIN crews on crews.id = person_crews.id").joins("INNER JOIN movies ON movies.id = crews.id AND movies.id = #{self.id}")
+    end
+
+    def get_crew(job, department)
+        Person.joins("JOIN person_crews ON person_crews.person_id = people.id AND person_crews.job = '#{job.capitalize}' AND person_crews.department = '#{department.capitalize}'").joins("INNER JOIN crews on crews.id = person_crews.id").joins("INNER JOIN movies ON movies.id = crews.id AND movies.id = #{self.id}")
+    end
 end
